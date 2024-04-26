c5ecdac6c2e32d220cbb5931db5f6c55bcc2eeaf
dir-FY24ayakakubota <ayaka.kubota.z489@dirbato.co.jp>
2024-04-26 19:32:57 +0900

    finish lesson 13

---
+++ techacademy_orm/src/action/DBUtil.java
@@ -0,0 +1,12 @@
+package action;
+
+import javax.persistence.EntityManager;
+
+public class DBUtil {
+
+    public static EntityManager createEntityManager() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+}
